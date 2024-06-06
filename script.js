651f12c0d959c52da0b39d5b6c3d9542c740dfd6
vishakh <vishakhhrithi007@gmail.com>
2024-06-06 21:37:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+var menubar = document.getElementById("menu");
+
+menubar.addEventListener("click", openMenu);
+
+var hide = document.getElementById("aside");
+
+var rot = document.getElementById("menu");
+
+function openMenu() {
+    hide.classList.toggle("hidden");
+    rot.classList.toggle("rotation");
+}
+
+function bg1(){
+document.body.style.backgroundImage="url(2022-Volkswagen-T-Roc-R-001-1440w.jpg)";
+}
+function bg2(){
+document.body.style.backgroundImage="url(2024-Tesla-Cybertruck-001-1440w.jpg)";
+}
+
+function bg3(){
+document.body.style.backgroundImage="url(2024-Audi-RS4-Avant-25-Years-Edition-006-1440w.jpg)";
+}
+function bg4(){
+document.body.style.backgroundImage="url(2023-BMW-M2-M-Performance-Parts-004-1440w.jpg)";
+}
+function bg5(){
+document.body.style.backgroundImage="url(13643.jpg)";
+}
+function bg6(){
+document.body.style.backgroundImage="url(2025-Lamborghini-Urus-SE-001-1440sw.jpg)";
+}
+function bg7(){
+document.body.style.backgroundImage="url(2022-Land-Rover-Range-Rover-002-1440w.jpg)";
+}
+function bg8(){
+document.body.style.backgroundImage="url(2023-Mercedes-AMG-C63-S-E-Performance-003-1440w.jpg)";
+}
+function bg9(){
+document.body.style.backgroundImage="url(mini.jpg)";
+}
+
+function bg10(){
+document.body.style.backgroundImage="url(2022-Porsche-911-Carrera-GTS-006-1440w.jpg)";
+}
